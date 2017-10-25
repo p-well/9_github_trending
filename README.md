68f6c9a09405774f097a4c7033adadeeb3a7bfe4
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-10-25 13:41:48 +0300

    Bug fixes in code and README updates

--- README.md
+++ README.md
@@ -19,9 +19,31 @@ It is recommended to use virtual environment.
 
 # Usage
 
-To execute the script use the following command in CLI: ```python github_trending.py```.
+To execute the script use the following command in CLI: ```python github_trending.py```. <br />
+CLI encoding changing may be required for correct script running on Windows platfrom: ```chcp 65001``` for UTF-8 encoding.
 
 # Example of Script Launch
+
+```Hey! Here are the most popular Python projects for the last week on GitHub.
+
+
+1. Repository name: wedding
+    Stars count: 738
+    Description: 婚礼大屏互动，微信请柬一站式解决方案
+    Issues count: 1
+    Link: https://github.com/iammapping/wedding
+
+2. Repository name: nyc-dna-software
+    Stars count: 264
+    Description: The source code, acquired by ProPublica, for New York City's Forensic Statistical Tool.
+    Issues count: 3
+    Link: https://github.com/propublica/nyc-dna-software
+
+3. Repository name: mx-maskrcnn
+    Stars count: 214
+    Description: A MXNet implementation of Mask R-CNN
+    Issues count: 0
+    Link: https://github.com/TuSimple/mx-maskrcnn ```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -14,17 +14,16 @@ def get_trending_repositories(top_size=20):
                         'order':'desc',
                         'per_page': top_size}
     try:
-        repo_responce = requests.get(repos_query_url, params = query_parameters)
+        repo_responce = requests.get(repo_query_url, params = query_parameters)
     except requests.exceptions.RequestException:
         repo_responce = None
     if repo_responce is not None:
         return repo_responce.json().get('items')
         
 def get_open_issues_amount(repo_owner, repo_name):
-    issue_query_url = 'https://api.github.com/repos/{}/{}/issues'
-                                                    .format(repo_owner, repo_name)
+    issue_query_url = 'https://api.github.com/repos/{}/{}/issues'.format(repo_owner, repo_name)
     try:
-        issue_responce = request.get(issue_query_url)
+        issue_responce = requests.get(issue_query_url)
     except requests.exceptions.RequestException:
         issue_responce = None
     if issue_responce is not None:
@@ -33,7 +32,7 @@ def get_open_issues_amount(repo_owner, repo_name):
 if __name__ == '__main__':
     top_repos_data = get_trending_repositories()
     if top_repos_data:
-        print('Hey! Here are the most popular Python projects for the last week on GitHub.'/n)  
+        print('\nHey! Here are the most popular Python projects for the last week on GitHub.\n')  
         for repo_count, repo_data in enumerate(top_repos_data, start = 1):
             print('''
 {}. Repository name: {}
