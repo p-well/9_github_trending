5b06d1f11ac84ffb4eaec3b5845460af484d1d30
Pavel <p.a.kadantsev@gmail.com>
2017-11-07 20:18:45 +0300

    Brought more PEP8 guides into code

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,13 @@
-import requests
 from datetime import datetime, timedelta
+import requests
+
 
 def get_week_ago_date():
     raw_week_ago_date = datetime.now() - timedelta(weeks = 1)
     formated_week_ago_date = raw_week_ago_date.strftime('%Y-%m-%d')
     return formated_week_ago_date
 
+    
 def get_trending_repositories(top_size=20):
     repo_query_url = 'https://api.github.com/search/repositories'
     query_parameters = {'q':'created:>{}'.format(get_week_ago_date()),
@@ -13,34 +15,45 @@ def get_trending_repositories(top_size=20):
                         'sort':'stars',
                         'order':'desc',
                         'per_page': top_size}
-    seconds_to_connect_break = 3.05                      
+    timeout_seconds = 3.05                      
     try:
-        repo_response = requests.get(repo_query_url, params = query_parameters, timeout = seconds_to_connect_break)
+        repo_response = requests.get(repo_query_url, \ 
+                                     params = query_parameters, \
+                                     timeout = timeout_seconds)
         return repo_response.json().get('items')
     except requests.exceptions.RequestException:
         repo_response = None
+
         
 def get_open_issues_amount(repo_owner, repo_name):
-    issue_query_url = 'https://api.github.com/repos/{}/{}/issues'.format(repo_owner, repo_name)
-    seconds_to_connect_break = 3.05
+    issue_query_url = 'https://api.github.com/repos/{}/{}/issues' \
+                       .format(repo_owner, repo_name)
+    timeout_seconds  = 3.05
     try:
-        issue_response = requests.get(issue_query_url, timeout = seconds_to_connect_break)
+        issue_response = requests.get(issue_query_url,\
+                                      timeout = timeout_seconds)
         return len(issue_response.json())
     except requests.exceptions.RequestException:
         issue_response = None
 
+        
 if __name__ == '__main__':
     top_repos_data = get_trending_repositories()
     if top_repos_data:
-        print('\nHey! Here are the most popular Python projects for the last week on GitHub.\n')  
+        print('\nHere are the most popular Python projects \
+               for the last week on GitHub.\n')  
         for repo_count, repo_data in enumerate(top_repos_data, start = 1):
+            owner_login = repo_data.get('owner').get('login')
+            owner_name = repo_data.get('name')
+            issues_amount = get_open_issues_amount(owner_login, owner_name)
             print('''
 {}. Repository name: {}
     Stars count: {}
     Description: {}
-    Issues count: {}
+    Issues amount: {}
     Link: {}'''.format(repo_count, repo_data.get('name'),
                        repo_data.get('stargazers_count'),
                        repo_data.get('description'),
-                       get_open_issues_amount(repo_data.get('owner').get('login'), repo_data.get('name')),
+                       issues_amount,
                        repo_data.get('html_url')))
+            
